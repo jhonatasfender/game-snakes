b9130304b91eb682fdc0ef7d8564e450fd4eff17
Jonatas Rodrigues <jhonatas.fender@gmail.com>
2016-06-03 02:27:39 -1100

    almost finished

--- snake01/main.js
+++ snake01/main.js
@@ -67,7 +67,67 @@ function init(){
 		}
 	},
 	updateSnake=function(){
+		//Update the position of the snake
+		var head_x = snake[0].x,head_y = snake[0].y,tail=snake.pop();
+		//Get the directions
+		document.onkeydown = function(e){
+			var key = e.keyCode;
+			if(key==37&&dir!="right")setTimeout(function(){dir="left";},30);
+			else if(key==38&&dir!="down")setTimeout(function(){dir="up";},30);
+			else if(key==39&&dir!="left")setTimeout(function(){dir="right";},30);
+			else if(key==40&&dir!="up")setTimeout(function(){dir="down";},30);
+			if(key)e.preventDefault();
+		}
+		//Directions
+		if(dir=="right")head_x++;
+		else if(dir=="left")head_x--;
+		else if(dir=="up")head_y--;
+		else if(dir=="down")head_y++;
+		//move snake
+		tail.x=head_x;
+		tail.y=head_y;
+		snake.unshift(tail);
+		if(head_x>=w/size||head_x<=-1||head_y>=h/size||head_y<=-1){
+			if(over==0){
+				hitType="wall";
+				gameOver();
+			}
+			over++;
+		}
+		//Food cllision
+		if(head_x==f.x&&head_y==f.y){
+			coll=1;
+			f=new Food();
+			var tail = {x:head_x,y:head_y};
+			snake.unshift(tail);
+			score+=10;
+			scoreText.innerHTML="Score: "+ score;
+			foodMusic.pause();
+			foodMusic.currentTime=0;
+			foodMusic.play();
+			//Increase speed
+			if(speed<=45)speed++;
+			clearInterval(game_loop);
+			game_loop=setInterval(draw,1000/spped);
+		}else{
+			//check collision between snake parts 
+			for(var j=1;j<snake.length;j++){
+				var s=snake[j];
+				if(head_x==s.x&&kead_y==s.y){
+					if(over==0){
+						hitType="self";
+						gameorver();
+					}
+					over++;
+				}
+			}
+		}
+	},
+	draw=function(){
+		paintCanvas();
+		paintSnake();
+		updateSnake();
+		// Draw food
 		
 	},
 }
-
