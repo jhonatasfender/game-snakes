1a8b0e28cf116810d2b398b5ddcd944066d365f7
Jonatas Rodrigues <jhonatas.fender@gmail.com>
2016-06-02 13:03:25 -1100

    Update main.js

--- snake01/main.js
+++ snake01/main.js
@@ -1,7 +1,13 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * first version I am basing myself on this link
+ * http://cssdeck.com/labs/classic-snake-game-with-html5-canvas
  */
-
+var mainMusic = document.getElementById("main_music"),
+foodMusic=document.getElementById("food"),
+goMusic=document.getElementById("gameOver"),
+start=document.getElementById("start")
+loading=document.getElementById("loading"),
+files=[mainMusic,foodMusic,goMusic],counter=0;
+for(var i=0;i<files.length){
+}
 
